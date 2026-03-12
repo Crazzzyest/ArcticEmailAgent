933becd8a715f0969c9c301422fbb27a00932bcf
Edson Reistad <edsongr@stud.ntnu.no>
2026-03-12 19:12:41 +0100

    initial commit

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
-from pydantic import BaseSettings, AnyHttpUrl
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
